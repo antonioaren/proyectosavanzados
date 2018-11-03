399ef129b074dfc6807f5b58d2c5aff69fc8400e
Pedro <antonioaren@hotmail.com>
2018-11-03 13:03:05 +0000

    Checking credential at firebase.

--- app/src/main/java/com/ulpgc/proyectosavanzados/pedroarenas/practica1/login/repository/LoginRepository.java
+++ app/src/main/java/com/ulpgc/proyectosavanzados/pedroarenas/practica1/login/repository/LoginRepository.java
@@ -1,4 +1,80 @@
 package com.ulpgc.proyectosavanzados.pedroarenas.practica1.login.repository;
 
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ulpgc.proyectosavanzados.pedroarenas.practica1.login.events.LoginEvent;
+
+import org.greenrobot.eventbus.EventBus;
+
+
 public class LoginRepository implements I_LoginRepository {
+
+    private FirebaseAuth mAuth;
+
+
+    public LoginRepository() {
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void singUp(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            postEvents(LoginEvent.SUCCESS_ALTA);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            postEvents(LoginEvent.ERROR_ALTA);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void singIn(String email, String password) {
+
+        try {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                //If login is ok
+                                Log.d("AUTENTICATION", "signInWithEmail:success");
+                                postEvents(LoginEvent.SUCCESS_LOG_IN);
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("AUTENTICATION", "signInWithEmail:failure", task.getException());
+                                postEvents(LoginEvent.ERROR_LOG_IN);
+
+                            }
+
+                        }
+                    });
+
+        } catch (Exception e) {
+            postEvents(LoginEvent.ERROR_LOG_IN);
+        }
+    }
+
+    @Override
+    public void forgotPass(String email) {
+
+    }
+
+    private void postEvents(int type) {
+        LoginEvent event = new LoginEvent();
+        event.setEventType(type);
+        EventBus.getDefault().post(event);
+
+    }
 }
